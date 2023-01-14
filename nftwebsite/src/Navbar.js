adc6396158a18c739cf163547979b91e43dd0a9a
Josip Harci <harcijosip99@gmail.com>
2023-01-14 23:15:02 +0100

    Update

--- nftwebsite/src/Navbar.js
+++ nftwebsite/src/Navbar.js
@@ -1,3 +1,4 @@
+
 import './Navbar.scss';
 import logo from './Img/logo.png'
 
@@ -6,6 +7,9 @@ const Navbar = () => {
         <div className='navbar'>
             <div className='logo_box'>
                 <img src={logo} alt="Logo"/>
+            </div>
+            <div className='nav_box'>
+                  <p>HOME</p>
             </div>
         </div>
     )
