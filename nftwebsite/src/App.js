12c4cb4d6160147b912bc75a05a0d7cb17b3ed4d
Josip Harci <harcijosip99@gmail.com>
2023-01-17 23:05:32 +0100

    Update

--- nftwebsite/src/App.js
+++ nftwebsite/src/App.js
@@ -1,6 +1,6 @@
 import './App.scss';
 import Navbar from './Navbar.js';
-import Border from './Img/img1.jpg'; 
+import Border from './Img/img1.png'; 
 
 function App() {
   return (
@@ -9,6 +9,10 @@ function App() {
       <div class="Container">
         <div class="Border">
             <img src={Border} alt="img"/>
+            <div class="Text">
+                <h2>Lucky Rabbit Club</h2>
+                <p>An Environment that is connecting Creative and like-minded people, communiti-driven project!</p>
+            </div>
         </div>
       </div>
     </div>
