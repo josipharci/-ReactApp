507a2164da291b6c96e3f129dfdfbb66b312c573
Josip Harci <harcijosip99@gmail.com>
2023-01-22 21:56:16 +0100

    Update

--- nftwebsite/src/components/Home/index.js
+++ nftwebsite/src/components/Home/index.js
@@ -4,9 +4,11 @@ import img from '../../assets/images/img1.png';
 const Home = () => {
     return (
       <div className="container_index">
-        <h2>Lucky Rabbit Club</h2>
-        <p>An Environment that is connecting <br/>Creative and like-minded people,<br/>communiti-driven project!</p>
-        <div className="img_box"><img src={img} alt="img"/></div>
+        <h2 className="flipY">Lucky Rabbit Club</h2>
+         <p id="one">An Environment that is connecting. </p>
+         <p id="two">Creative and like-minded people,</p>
+         <p id="three">communiti-driven project!</p>  
+        <div className="img_box"><img  src={img} alt="img"/></div>
       </div>
     );
 }
