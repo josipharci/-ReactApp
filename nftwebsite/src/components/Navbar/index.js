9d2441750cbfae332df9216311212082cdbb6139
Josip Harci <harcijosip99@gmail.com>
2023-01-24 23:23:26 +0100

    Update

--- nftwebsite/src/components/Navbar/index.js
+++ nftwebsite/src/components/Navbar/index.js
@@ -1,14 +1,24 @@
 import './index.scss';
-//import { Link,NavLink } from 'react-router-dom';
-//import Logo from '../../assets/images/logo.png';
-//import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-//import {faHome , faUser , faEnvelope} from '@fortawesome/free-solid-svg-icons';
-
+import Logo from '../../assets/images/logo.png';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHome , faUser , faComment} from '@fortawesome/free-solid-svg-icons';
+import {faFacebook,faLinkedin} from '@fortawesome/free-brands-svg-icons';
 const Navbar = () => {
    return (
      <>
      <nav>
-
+        <a href="/">
+          <img class="logo" src={Logo} alt="Logo_img"/>
+        </a>
+        <div>
+          <ul id="navbar">
+            <li><a href="/"><FontAwesomeIcon class="icon" icon={faHome} color='#fff'/>Home</a></li>
+            <li><a href="/"><FontAwesomeIcon class="icon" icon={faUser} color='#fff'/>About</a></li>
+            <li><a href="/"><FontAwesomeIcon class="icon" icon={faComment} color='#fff'/>Contact</a></li>   
+            <li class="social_link"><a href="/"><FontAwesomeIcon icon={faFacebook} color='#fff'/></a></li>    
+            <li class="social_link"><a href="/"><FontAwesomeIcon icon={faLinkedin} color='#fff'/></a></li>       
+          </ul>
+        </div>
      </nav>
      </>
    );
