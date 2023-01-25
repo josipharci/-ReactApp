21007768ac1caa6a8a97d815f5804e687b9cf387
Josip Harci <harcijosip99@gmail.com>
2023-01-25 13:23:12 +0100

    Update

--- nftwebsite/src/components/Navbar/index.js
+++ nftwebsite/src/components/Navbar/index.js
@@ -20,11 +20,13 @@ class Navbar extends
           <img class="logo" src={Logo} alt="Logo_img"/>
         </a>
         <div>
-          <ul id="navbar">
+          <ul id="navbar" className={
+            this.state.clicked ? "#navbar active"
+          : "#navbar"}>
             <li><a href="/"><FontAwesomeIcon class="icon" icon={faHome} color='#fff' activeclassname='active'/>Home</a></li>
             <li><a href="/About"><FontAwesomeIcon class="icon" icon={faUser} color='#fff' activeclassname='active'/>About</a></li>
             <li><a href="/Contact"><FontAwesomeIcon class="icon" icon={faComment} color='#fff' activeclassname='active'/>Contact</a></li>   
-            <li class="social_link"><a href="/"><FontAwesomeIcon icon={faFacebook} color='#fff'/></a></li>    
+            <li class="social_link"><a href="https://www.facebook.com/profile.php?id=100009919082627"><FontAwesomeIcon icon={faFacebook} color='#fff'/></a></li>    
             <li class="social_link"><a href="/"><FontAwesomeIcon icon={faLinkedin} color='#fff'/></a></li>       
           </ul>
         </div>
