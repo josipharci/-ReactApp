48e742bf755da26a311cabfc0d47b76e5e2175fa
Josip Harci <harcijosip99@gmail.com>
2023-01-26 13:30:38 +0100

    Update

--- nftwebsite/src/components/Home/index.js
+++ nftwebsite/src/components/Home/index.js
@@ -1,10 +1,23 @@
 import './index.scss';
-// import img from '../../assets/images/img1.png';      
+import img from '../../assets/images/nft_1.png';  
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+
 
 const Home = () => {
     return (
       <>
-       
+       <div class="index">
+            <h2>Lucky Rabbit Club</h2>
+            <p>An Environment that is connecting Creative and like-minded people, communiti-driven project!</p>
+            <a 
+             href='/About'
+             target="_blank"
+             rel='noreferrer'>
+             <FontAwesomeIcon class="icon" icon={faArrowRight} color='#fff'></FontAwesomeIcon>NEXT
+            </a>
+            <img src={img} alt="img"/>
+       </div>
       </>
     );
 }
