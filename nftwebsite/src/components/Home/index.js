eecb686a986b182496bc8181d437465b16e4d54b
Josip Harci <harcijosip99@gmail.com>
2023-02-07 19:34:04 +0100

    Update

--- nftwebsite/src/components/Home/index.js
+++ nftwebsite/src/components/Home/index.js
@@ -1,55 +1,11 @@
 import './index.scss';
-import img from '../../assets/images/nft_1.png';  
-import img2 from '../../assets/images/nft_2.png';  
+//import img from '../../assets/images/nft_1.png';  
+//import img2 from '../../assets/images/nft_2.png';  
 
 const Home = () => {
     return (
       <>
-        <div className="container">
-           <div className="col">
-              <h2 className="header">Lucky Rabbit Club</h2>
-              <p className="text">An Environment that is connecting<br/>Creative and like-minded people,<br/>communiti-driven project!</p>
-           </div>
-           <div className="col">
-              <img src={img} alt="img" />
-           </div>
-        </div>
-        <div className="about_container">
-        <h2 className="header_about">ABOUT</h2>
-           <p className="text_2">
-            The Lucky Rabbit Club is a series of high-quality, unique,<br/>
-            and col lectible non-fungible tokens (NFTs) featuring the <br/>
-            artwork of the Lucky Rabbit Collective! Lucky Rabbit Club <br/>
-            was created becouse we are tired of so much rug pulls and <br/>
-            useless staking for earning ponzi coin! <br/>
-           </p>
-           <p className='text_3'>
-            The Lucky Rabbit team created the NFT project as a way to share <br/>
-            their beautiful artwork with collectors and fans around the world.<br/>
-            The NFTs allow collectors to own a truly one-of-a-kind piece and <br/>
-            support the artists in their work. We have added a new trade nexus <br/>
-            tool to our server.(Which will be available until mint, after mint <br/>
-            tools will be available only to holders).<br/>
-           </p>
-           <p className="text_4">
-            This tool includes analytics, mints, trackers, <br/>
-            and LMNFT capabilities. We are confident that this new addition will <br/>
-            enhance your trading experience and provide valuable insights.<br/>
-            The Lucky Rabbit NFTs will be available for mint and purchase through <br/>
-            various online marketplaces and platforms.<br/>
-           </p>
-           <p className="text_5">
-            Keep an eye out for announcements from the<br/>
-            Lucky Rabbit Collective on social media or their website<br/>
-            for details on how and where to purchase the NFTs.  <br/>
-            We always continue to build, grow our community, giveaways,<br/>
-            holders rewards and some promotions on socials! Website almoust ready to<br/>
-            go live and we have some BIIG NEWS for y'all... Stay tuned for updates!<br/>
-           </p>
-           <div className="img">
-           <img className="img_2" src={img2} alt="img"/>
-           </div>
-        </div>
+          
       </>
     );
 }
--- nftwebsite/src/components/Navbar/index.js
+++ nftwebsite/src/components/Navbar/index.js
@@ -1,48 +1,17 @@
 import './index.scss';
-import Logo from '../../assets/images/logo.png';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHome ,faUser,faComment,faBars, faTimes, faImage } from '@fortawesome/free-solid-svg-icons';
-import {faDiscord, faTwitter} from '@fortawesome/free-brands-svg-icons';
-import { Component } from 'react';
 
-class Navbar extends 
- Component {
-  state={ clicked: false };
-  handleClick = () =>{
-    this.setState({clicked:
-    !this.state.clicked})
-  }
-   render(){
-   return (
-     <>
-     <nav className='nav'>
-        <a className='link' href="/">
-          <img class="logo" src={Logo} alt="Logo_img"/>
-        </a>
-        <div>
-          <ul id="navbar" className={
-            this.state.clicked ? "#navbar active"
-          : "#navbar"}>
-            <li><a href="/"><FontAwesomeIcon class="icon" icon={faHome} color='#fff' activeclassname='active'/>Home</a></li>
-            <li><a href="/About"><FontAwesomeIcon class="icon" icon={faUser} color='#fff' activeclassname='active'/>About</a></li>
-            <li><a href="/Contact"><FontAwesomeIcon class="icon" icon={faComment} color='#fff' activeclassname='active'/>Contact</a></li>   
-            <li><a href="/Gallery"><FontAwesomeIcon class="icon" icon={faImage} color='#fff' activeclassname='active'/>Gallery</a></li>   
-            <li className="social_link"><a href="https://twitter.com/LRabbitClub?t=mwO0aXibbNBFDws16DL38Q&s=09"><FontAwesomeIcon icon={faTwitter} color='#fff'/></a></li>    
-            <li className="social_link"><a href="https://discord.gg/JGzqbhGsmM"><FontAwesomeIcon icon={faDiscord} color='#fff'/></a></li>       
+const Navbar = () => {
+  return (
+    <>
+       <nav className="navbar">
+          <div className="col logo">
+             <img src="#" alt="#"/>
+          </div>
+          <ul className="col links">
           </ul>
-        </div>
-        <div id="mobile" onClick={this.handleClick}>
-          <FontAwesomeIcon id="icon_mobile" icon={
-            this.state.clicked ? 
-            faTimes : 
-            faBars
-          }/>
-          
-        </div>
-     </nav>
-     </>
-   );
- }
+         </nav>
+    </>
+  );
 }
 
 export default Navbar;
