9d3f87e7464bb9c5c6c221ef7c4751c27a0cf9dc
Josip Harci <harcijosip99@gmail.com>
2023-02-07 21:58:13 +0100

    Update

--- nftwebsite/src/components/Home/index.js
+++ nftwebsite/src/components/Home/index.js
@@ -1,5 +1,5 @@
 import './index.scss';
-//import img from '../../assets/images/nft_1.png';  
+import img from '../../assets/images/nft_1.png';  
 //import img2 from '../../assets/images/nft_2.png';  
 
 const Home = () => {
@@ -8,6 +8,8 @@ const Home = () => {
          <div className="container header">
             <div className="centered">
                <h1>Lucky Rabbit Club</h1>
+               <p>An Environment that is connecting<br/>Creative and like-minded people,<br/>communiti-driven project!</p>
+               <img className="nft_1" src={img} alt="nft1" />
             </div>
          </div> 
       </>
